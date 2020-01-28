ea3f14529ea483749d84d3c4555914ec7b43b071
Lucas <lwolfs@my.wctc.edu>
2020-01-27 18:46:10 -0600

    EF Core configured
    Created DB classes, Configured EF Core, Created Initial DB and populated with Seed Data

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Modas.Models
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        //corresponds with events table
+        public DbSet<Event> Events { get; set; }
+        //corresponds with locations table
+        public DbSet<Location> Locations { get; set; }
+    }
+}
